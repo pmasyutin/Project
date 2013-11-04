99aec0be204c8343f4a20a7fd4524cb62a9a3bb7
Pavel Masyutin <pmasyutin@massmo.ru>
2013-11-04 23:32:44 +0400

    Int

---
+++ expense/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Expense::Application.config.secret_token = '1c733c002cabdf72278422c59341829a896b34d4cfaf58060e77f5f0e687d1920e98303c02747a6578e1f1d8c1af88b7d9864268d77e6e7a041415f198f336d9'
